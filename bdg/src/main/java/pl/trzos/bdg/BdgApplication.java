d5f7c0dcb5aa1a0d7cda5f2e218345038124908d
-v <wtrzos@gmail.com>
2019-10-18 21:59:34 +0200

    initial comit

---
+++ bdg/src/main/java/pl/trzos/bdg/BdgApplication.java
@@ -0,0 +1,13 @@
+package pl.trzos.bdg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdgApplication.class, args);
+	}
+
+}
---
+++ bdg/src/test/java/pl/trzos/bdg/BdgApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.trzos.bdg;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BdgApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
